57e098d45dd822ccac29a9516d0df6cda687baac
Ashish Kumar Palai <ashishkumarpalai2000@gmail.com>
2023-02-19 18:09:20 +0530

    update

--- index.js
+++ index.js
@@ -6,12 +6,38 @@ const { userRouter } = require("./routes/user.routes")
 const { noteRouter } = require("./routes/note.routes")
 const { authenticate } = require("./middlewares/authenticate.middlewares")
 const cors=require("cors")
-
+//swagger start
+const swaggerUI=require("swagger-ui-express")
+const swaggerJsDoc=require("swagger-jsdoc")
+//swagger end
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+//open api specifications   ====>   start
+const options={
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title:"Learning swagger for first time",
+            version:"1.0.0"
+        },
+        server:[
+            {
+                url: "http://localhost:9874"
+            }
+        ]
+    },
+    apis:["./routes/*.js"]
+}
+//swagger specs
+const swaggerSpec=swaggerJsDoc(options)
+//build the UI
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec))
+//open api specifications   ====>   end
+
 
 app.get("/", (req, res) => {
     res.send("home page")
